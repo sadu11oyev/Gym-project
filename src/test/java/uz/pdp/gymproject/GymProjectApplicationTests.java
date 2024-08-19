a92470563e958c873664a85a968c474a4541deec
Бахтиёр Садуллоев <baxtisadulloyev@gmail.com>
2024-08-19 16:19:12 +0500

    first commit

---
+++ src/test/java/uz/pdp/gymproject/GymProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.gymproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
